c7d01bb1f5c81c6fc2d672bacbd2bdf234beec1a
MazDot <hamedmaz400@gmail.com>
2023-01-29 21:19:35 +0330

    Add project files.

---
+++ signalrtest/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using signalrtest.Data;
+
+namespace signalrtest.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly ApplicationDbContext _db;
+        public ChatHub(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
+        public async Task SendMessageToReceiver(string sender, string receiver, string message)
+        {
+            var userId = _db.Users.FirstOrDefault(u => u.Email.ToLower() == receiver.ToLower()).Id;
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                await Clients.User(userId).SendAsync("MessageReceived", sender, message);
+
+            }
+
+        }
+    }
+}
---
+++ signalrtest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace signalrtest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
